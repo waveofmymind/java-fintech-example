a507e14e0a1a6166939407a05184f857a8876526
waveofmymind <sjun0913@gmail.com>
2023-11-17 21:49:01 +0900

    feat: 기본 프로젝트 파일을 세팅한다.

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "kotlin-fintech-example"
---
+++ src/main/kotlin/com/waveofmymind/fintech/FintechApplication.kt
@@ -0,0 +1,11 @@
+package com.waveofmymind.fintech
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FintechApplication
+
+fun main(args: Array<String>) {
+    runApplication<FintechApplication>(*args)
+}
