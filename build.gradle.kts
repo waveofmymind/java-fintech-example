417e9f668f20bfbdf38c0450e4a585bd5ac4decb
waveofmymind <sjun0913@gmail.com>
2023-11-19 01:19:55 +0900

    feat(root): kotlin 관련 의존성을 추가한다.

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,12 @@ subprojects {
 
     dependencies {
         implementation("org.springframework.boot:spring-boot-starter")
+
+        // kotlin
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
         implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
         testImplementation("org.springframework.boot:spring-boot-starter-test")
     }
 
