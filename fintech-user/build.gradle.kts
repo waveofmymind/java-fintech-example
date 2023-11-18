5d1fa6a17ee67290e9afbc691e0fa9219e5a3e6e
waveofmymind <sjun0913@gmail.com>
2023-11-19 02:09:40 +0900

    feat(user): feign 의존성을 추가한다.

--- fintech-user/build.gradle.kts
+++ fintech-user/build.gradle.kts
@@ -7,10 +7,20 @@ tasks.getByName<Jar>("jar") {
     enabled = false
 }
 
+extra["springCloudVersion"] = "2022.0.4"
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+
     runtimeOnly("com.mysql:mysql-connector-j")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
 }
