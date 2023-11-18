68d7b73fc41df2de5b0b979890ed538c38685c31
waveofmymind <sjun0913@gmail.com>
2023-11-19 01:21:18 +0900

    feat(user): mysql 의존성을 추가한다.

--- fintech-user/build.gradle.kts
+++ fintech-user/build.gradle.kts
@@ -11,4 +11,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    runtimeOnly("com.mysql:mysql-connector-j")
 }
