0d9c1dde47baddfdfb05a8e4d374edc7afa94c05
waveofmymind <sjun0913@gmail.com>
2023-12-01 00:01:37 +0900

    feat(gateway): ExceptionHandler를 구현한다.

--- fintech-gateway/src/main/kotlin/com/waveofmymind/gateway/filter/JwtAuthorizationFilter.kt
+++ fintech-gateway/src/main/kotlin/com/waveofmymind/gateway/filter/JwtAuthorizationFilter.kt
@@ -1,11 +1,16 @@
 package com.waveofmymind.gateway.filter
 
+import com.waveofmymind.gateway.jwt.JwtException
 import com.waveofmymind.gateway.jwt.JwtVerifier
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler
 import org.springframework.cloud.gateway.filter.GatewayFilter
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory
+import org.springframework.context.annotation.Bean
 import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Component
 import org.springframework.web.server.ServerWebExchange
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @Component
 class JwtAuthenticationGatewayFilter : AbstractGatewayFilterFactory<
@@ -28,5 +33,30 @@ class JwtAuthenticationGatewayFilter : AbstractGatewayFilterFactory<
         exchange.mutate().request(
             exchange.request.mutate().header("X-Authorization-id", userId).build()
         ).build()
+    }
+
+    @Bean
+    fun tokenValidation(): ErrorWebExceptionHandler {
+        return JwtExceptionHandler()
+    }
+
+    class JwtExceptionHandler : ErrorWebExceptionHandler {
+        private fun getErrorCode(errorCode: Int) = "에러 코드: $errorCode"
+
+        override fun handle(exchange: ServerWebExchange, ex: Throwable): Mono<Void> {
+            var errorCode = 500
+            when (ex) {
+                is JwtException -> {
+                    errorCode = 401
+                }
+
+                is IllegalArgumentException -> {
+                    errorCode = 400
+                }
+            }
+            val bytes = getErrorCode(errorCode).toByteArray()
+            val buffer = exchange.response.bufferFactory().wrap(bytes)
+            return exchange.response.writeWith(Flux.just(buffer))
+        }
     }
 }
